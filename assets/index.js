6a644a8daa814222f8e0f6463442cf2fc19de866
chies231 <kozwom@gmail.com>
2025-04-11 22:41:56 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,17 @@
-let upload;
-let imageInput;
-let idOwnImage;
+document.querySelectorAll(".date_input").forEach((element) => {
+    element.addEventListener('click', () => {
+        document.querySelector(".date").classList.remove("error_shown")
+    })
+})
+
+var sex = "m"
+
+document.querySelectorAll(".selector_option").forEach((option) => {
+    option.addEventListener('click', () => {
+        sex = option.id;
+        document.querySelector(".selected_text").innerHTML = option.innerHTML;
+    })
+})
 
 var upload = document.querySelector(".upload");
 
@@ -54,75 +65,61 @@ imageInput.addEventListener('change', (event) => {
 
 })
 
-var selector = document.querySelector(".selector_box");
-selector.addEventListener('click', () => {
-    selector.classList.toggle("selector_open");
-});
+document.querySelector(".go").addEventListener('click', () => {
 
-document.querySelectorAll(".date_input").forEach((element) => {
-    element.addEventListener('click', () => {
-        document.querySelector(".date").classList.remove("error_shown");
-    });
-});
-
-var sex = "m";
-document.querySelectorAll(".selector_option").forEach((option) => {
-    option.addEventListener('click', () => {
-        sex = option.id;
-        document.querySelector(".selected_text").innerHTML = option.innerHTML;
-    });
-});
-
-document.querySelectorAll(".input_holder").forEach((element) => {
-    const input = element.querySelector(".input");
-    input.addEventListener('click', () => {
-        element.classList.remove("error_shown");
-    });
-});
-
-document.querySelector(".go").addEventListener('click', processForm);
-
-function processForm() {
     var empty = [];
+
     var params = new URLSearchParams();
+
+    params.set("sex", sex)
+    if (!upload.hasAttribute("selected")){
+        empty.push(upload);
+        upload.classList.add("error_shown")
+    }else{
+        params.set("image", upload.getAttribute("selected"))
+    }
 
     var birthday = "";
     var dateEmpty = false;
     document.querySelectorAll(".date_input").forEach((element) => {
-        birthday = birthday + "." + element.value;
-        if (isEmpty(element.value)) {
+        birthday = birthday + "." + element.value
+        if (isEmpty(element.value)){
             dateEmpty = true;
         }
-    });
+    })
 
     birthday = birthday.substring(1);
 
-    if (dateEmpty) {
+    if (dateEmpty){
         var dateElement = document.querySelector(".date");
         dateElement.classList.add("error_shown");
         empty.push(dateElement);
-    } else {
-        params.set("birthday", birthday);
-        params.set("sex", sex);
+    }else{
+        params.set("birthday", birthday)
     }
 
     document.querySelectorAll(".input_holder").forEach((element) => {
+
         var input = element.querySelector(".input");
-        if (isEmpty(input.value)) {
+
+        if (isEmpty(input.value)){
             empty.push(element);
             element.classList.add("error_shown");
-        } else {
-            params.set(input.id, input.value);
-            localStorage.setItem(`input_${input.id}`, input.value);
+        }else{
+            params.set(input.id, input.value)
         }
-    });
 
-    if (empty.length === 0) {
+    })
+
+    if (empty.length != 0){
+        empty[0].scrollIntoView();
+    }else{
+
         forwardToId(params);
-    } else {
-        empty[0].scrollIntoView({ behavior: 'smooth' });
     }
-}
+
+});
+
 
 function forwardToId(params) {
     const formData = {
